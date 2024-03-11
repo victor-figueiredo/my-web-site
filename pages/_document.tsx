abf26755a0dc98abd62d9aaffa8fd0d94c3eb716
victor-figueiredo <vcfo081@gmail.com>
2024-03-10 21:38:38 -0300

    feat: add title and description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Victor Figueiredo Website</title>
+        <meta name="description" content="Desenvolvedor Frontend" />
+      </Head>
       <body className="font-primary">
         <Main />
         <NextScript />
