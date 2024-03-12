efa7ed24c0d1b71b64c6b7f06bed48c241913f91
victor-figueiredo <vcfo081@gmail.com>
2024-03-12 01:56:40 -0300

    feat: new personal study project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,55 +2,57 @@ import Image from "next/image";
 import React from "react";
 
 const Projects = () => {
+  const [showingTitle, setShowingTitle] = React.useState(0);
+  const myProjects = [
+    {
+      id: 1,
+      name: "Meu primeiro portfólio",
+      image: "/assets/images/p1.png",
+    },
+    {
+      id: 2,
+      name: "Chat com IA",
+      image: "/assets/images/p2.png",
+    },
+  ];
+
+  const showTitle = (projectId: number) => setShowingTitle(projectId);
+  const hideTitle = () => setShowingTitle(0);
+
   return (
     <div
       id="projects"
-      className="bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem]"
+      className="bg-[#02050a] pb-[1rem] pt-[4rem] md:pt-[8rem]"
     >
       <h1 className="heading">
         Pro<span className="text-[#5B7AC4]">jetos</span>
       </h1>
-      <div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
-        <div data-aos="fade-up">
-          <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
-            <Image
-              src="/assets/images/p1.png"
-              alt="portfolio"
-              layout="fill"
-              className="object-contain p-[8px] hover:border-[#fff] hover:border-[2px] hover:border-solid"
-            />
+      <div className="mx-auto grid w-[80%] grid-cols-1 gap-[2rem] pt-[2rem] md:grid-cols-2 lg:grid-cols-3">
+        {myProjects.map((project) => (
+          <div
+            data-aos="fade-up"
+            key={project.id}
+            onMouseEnter={() => showTitle(project.id)}
+            onMouseLeave={hideTitle}
+          >
+            <div className="relative h-[200px] w-[100%] transform cursor-pointer transition-all duration-200 md:h-[300px]">
+              {showingTitle === project.id && (
+                <p
+                  data-aos="fade-up"
+                  className="w-[100%] text-center text-white"
+                >
+                  {project.name}
+                </p>
+              )}
+              <Image
+                src={project.image}
+                alt={project.name}
+                layout="fill"
+                className="object-contain"
+              />
+            </div>
           </div>
-        </div>
-        {/* <div data-aos="fade-up" data-aos-delay="300">
-          <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
-            <Image
-              src="/assets/images/p1.jpg"
-              alt="portfolio"
-              layout="fill"
-              className="object-contain"
-            />
-          </div>
-        </div>
-        <div data-aos="fade-up" data-aos-delay="900">
-          <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
-            <Image
-              src="/assets/images/p1.jpg"
-              alt="portfolio"
-              layout="fill"
-              className="object-contain"
-            />
-          </div>
-        </div>
-        <div data-aos="fade-up" data-aos-delay="1200">
-          <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
-            <Image
-              src="/assets/images/p1.jpg"
-              alt="portfolio"
-              layout="fill"
-              className="object-contain"
-            />
-          </div>
-        </div> */}
+        ))}
       </div>
     </div>
   );
