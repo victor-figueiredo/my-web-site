d63aa68159e4fb8b766066e4c204da40ddb42ecc
Victor Figueiredo <vcfo081@gmail.com>
2024-03-24 19:54:16 -0300

    fix: rerender textEffect to translate

--- components/TextEffect.tsx
+++ components/TextEffect.tsx
@@ -1,10 +1,20 @@
-import { useTranslations } from 'next-intl'
+import { useTranslations, useLocale } from 'next-intl'
 import { TypeAnimation } from 'react-type-animation'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 const TextEffect = () => {
   const t = useTranslations('TextEffect')
+  const { locale } = useRouter()
+  const [key, setKey] = useState(0)
+
+  useEffect(() => {
+    setKey((prevKey) => prevKey + 1)
+  }, [locale])
+
   return (
     <TypeAnimation
+      key={key}
       sequence={[t('wordOne'), 1500, t('wordTwo'), 1500]}
       speed={50}
       className="text-[2rem] font-bold uppercase text-[#5B7AC4] md:text-[3rem]"
